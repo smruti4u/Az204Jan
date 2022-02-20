e6ebb7004d03bc016e9010e1aee954d1b1c7f892
Smruti Ranjan Panda <smrutienator@gmail.com>
2022-02-20 22:08:11 +0530

    Update To Authentication

---
+++ AuthenticationWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthenticationWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
